c39fc91f57b0abdc3818a9ab90187f8a73911441
Arul <hariarul2002@gmail.com>
2024-05-26 15:05:31 +0530

    LinearSearchDone

---
+++ LinearSearcj.js
@@ -0,0 +1,10 @@
+let find=(arr,key)=>{
+for(let elem of arr){
+    if(elem===key){
+        return true;
+    }
+}
+return false;
+}
+console.log(find([200,400,600,700],400));
+console.log(find([2,4,6,8,10],5));
